47b3342172cd4205132b086430bd0a0c37351451
swag100 <ti.n.tripp@baypath.net>
2025-06-25 02:39:12 -0400

    Many notes for tomorrow + move index to src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 // dot env config
@@ -16,6 +17,11 @@ const connection = mysql.createConnection({
 
 //awesome
 app.set('view engine', 'pug');
+app.use(session({
+    secret: process.env.SESS_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(express.static('public'));
 
 app.listen(process.env.PORT, () => {
@@ -26,12 +32,25 @@ app.get('/', (req, res) => {
 });
 
 //WE CAN QUERY THE DATABASE WOOOHOOOO
-/*
-connection.connect();
+/*connection.connect();
 connection.query("SELECT * FROM user", (err, rows, fields) => {
     if (err) throw err;
 
     console.log('The solution is: ', rows[0]);
 });
-connection.end();
+connection.end();*/
+//useing sessions
+/*app.post('/get-data', (req, res) => {
+    const username = req.session.username;
+    const loggedIn = req.session.loggedIn;
+    res.send(`Username: ${username}, Logged In: ${loggedIn}`);
+});*/
+
+/* Some Interesting Thoughts To Consider */
+/*
+Clients send INPUTS to the server, and the server does the logic for every player
+Prevents clients from hacking, but seems intense on server
+
+Also, where would a good place be for game server logic?
+Like how would i make it ONLY be happening on pages with a canvas?
 */
